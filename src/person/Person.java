9ffad28143a154633586dc2b4caaccb0c2bba24d
lukasz-b <lukasz_b@live.com>
2013-01-19 23:37:34 +0000

    Removed unnecessary constructors

--- src/person/Person.java
+++ src/person/Person.java
@@ -8,16 +8,5 @@ public class Person {
     private String m_name;
     private int m_id;
     private String m_email;
-    /*
-    public Person()
-    {
-        this( "Default", 0, "email@dot.com" );
-    }
 
-    public Person( String name, int id, String email )
-    {
-        this.m_name = name;
-        this.m_id = id;
-        this.m_email = email;
-    }        */
 }
